cbc2d49008b4c9e321ef59dabff2b9c5fe89f266
Cookiegithubby <vbf2003@gmail.com>
2024-09-26 10:39:58 +0200

    Lavet Victornavn class som har en funktion der udskriver navn, opdateret program.cs til at kalde victor og markas funktioner.

---
+++ ConsoleApp1/Victornavn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Victornavn
+    {
+        public static void UdskrivNavn()
+        {
+            Console.WriteLine("Hej, jeg hedder Victor!");
+        }
+    }
+}
